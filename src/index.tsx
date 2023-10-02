7a9917c81f246495e752dd25c45d6177ed123f97
Eugene Varnashin <NevadaRU@yandex.ru>
2023-10-02 14:15:28 +0300

    Add react

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { render } from 'react-dom'
+import { Counter } from './components/Counter'
 
 render(
-    <div>as232323da</div>,
+    <div>
+        <Counter />
+    </div>,
     document.getElementById('root')
 )
