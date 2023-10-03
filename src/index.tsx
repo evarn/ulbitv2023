14e7a5349ec612b51e5499c22bfdd520749d09ad
Eugene Varnashin <NevadaRU@yandex.ru>
2023-10-03 11:44:12 +0300

    add i18

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { ThemeProvide } from 'app/providers/ThemeProvider'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
+import './shared/config/i18n/i18n';
 
 render(
     <BrowserRouter>
