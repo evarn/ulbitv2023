8c2cb05b6281c07f86e50eefa0a6aa7bbe3aef77
Eugene Varnashin <NevadaRU@yandex.ru>
2023-10-04 11:38:04 +0300

    fix test

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -1,6 +1,5 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
-import { userEvent } from '@storybook/testing-library';
 import { Counter } from './Counter';
 
 describe('Counter', () => {
@@ -15,7 +14,7 @@ describe('Counter', () => {
         componentRender(<Counter />, {
             initialState: { counter: { value: 10 } },
         });
-        userEvent.click(screen.getByTestId('increment-btn'));
+        fireEvent.click(screen.getByTestId('increment-btn'));
         expect(screen.getByTestId('value-title')).toHaveTextContent('11');
     });
 
@@ -23,7 +22,7 @@ describe('Counter', () => {
         componentRender(<Counter />, {
             initialState: { counter: { value: 10 } },
         });
-        userEvent.click(screen.getByTestId('decrement-btn'));
+        fireEvent.click(screen.getByTestId('decrement-btn'));
         expect(screen.getByTestId('value-title')).toHaveTextContent('9');
     });
 });
