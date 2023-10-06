3f5f6364fd854045fa1c7e770854812ee5250d6d
Eugene Varnashin <NevadaRU@yandex.ru>
2023-10-06 12:55:02 +0300

    fix async component

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
+import webpack from 'webpack';
 import path from 'path';
-import type webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { type BuildEnv, type BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
@@ -9,6 +9,8 @@ export default (env: BuildEnv) => {
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
+        locales: path.resolve(__dirname, 'public', 'locales'),
+        buildLocales: path.resolve(__dirname, 'build', 'locales'),
     };
 
     const mode = env.mode || 'development';
@@ -25,5 +27,6 @@ export default (env: BuildEnv) => {
         apiUrl,
         project: 'frontend',
     });
+
     return config;
 };
